0a16767358ec5dd06850fec18139f4ce35e9631c
cake <liveinfantasy@gmail.com>
2019-02-05 20:21:20 -0500

    String reverser

---
+++ Day1/Exercises/03-iterations-1/ans.py
@@ -0,0 +1,6 @@
+# * Write a function that will take in any string and return an output that is the string in reverse
+
+def reverse_string(strng):
+    return strng[::-1]
+
+print(reverse_string('This is a test'))
